51db81567ddf29c2ab6566eea63e031d134310b1
Alan dev <aland3v@gmail.com>
2021-01-05 22:05:10 -0400

    ES7 includes y exponentes con '**'

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+let numbers = [1, 2, 3, 7, 8];
+
+// es7 -> includes es nuevo
+if (numbers.includes(4)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra el valor 7.');
+}
+
+// es7 -> elevar a potencias
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
