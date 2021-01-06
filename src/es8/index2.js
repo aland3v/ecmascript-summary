22339bfe51da6ca88ba397e6e7baa629f89287b7
Alan dev <aland3v@gmail.com>
2021-01-06 07:52:15 -0400

    ES8 Async y Await

---
+++ src/es8/index2.js
@@ -0,0 +1,26 @@
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    });
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    const hello2 = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
