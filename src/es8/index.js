aa8ccf45a5d7c9008572a668d74a57fe0022becf
Alan dev <aland3v@gmail.com>
2021-01-06 07:23:49 -0400

    Object entries, Object values, Padding, Trailing comas

---
+++ src/es8/index.js
@@ -0,0 +1,32 @@
+const data = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+// Object entries -> convierte Objeto a matriz
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const data2 = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+// Object Values -> los valores de Objeto a array
+const values = Object.values(data2);
+console.log(values);
+console.log(values.length);
+
+// Padding: casi como concatenar pero defino la longitud maxima
+const string = 'hello';
+console.log(string.padStart(7,'hi'))
+console.log(string.padEnd(12, ' -----*'));
+console.log('food'.padEnd(12, '  -----'));
+
+// Trailing comas, siempre con compas :D
+const obj = {
+    name: 'alan', // es mejor poner la coma
+}
