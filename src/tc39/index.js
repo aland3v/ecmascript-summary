62cfb8382e3bd815472f49c8557c30c724a8990a
Alan dev <aland3v@gmail.com>
2021-01-06 14:21:26 -0400

    tc39 comite encargado de las versiones js

---
+++ src/tc39/index.js
@@ -0,0 +1,13 @@
+`
+TC39
+Comite que recibe las propuestas y
+evaluarlas para llevarlas a la siguiente version
+donde vamos a poderla utilizar en un ESX(6,7,8,9,10,11)
+
+Sitio Web
+tc39.es
+
+Podemos contribuir y ser parte de las discusiones,
+encontrar todas las propuestas, estado de las
+propuestas.
+`
